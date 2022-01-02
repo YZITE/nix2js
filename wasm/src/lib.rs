de70add858a206fb614f2a4060ba954477fc7f46
zseri <zseri.devel@ytrizja.de>
2022-01-02 09:34:30 +0100

    wasm: fix typescript signature

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,9 +1,16 @@
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, JsCast};
 
 #[wasm_bindgen]
-pub fn translate(s: &str, inp_name: &str) -> Result<JsValue, JsValue> {
+extern "C" {
+    #[wasm_bindgen(typescript_type = "[string, string]")]
+    pub type TwoStrings;
+}
+
+#[wasm_bindgen]
+pub fn translate(s: &str, inp_name: &str) -> Result<TwoStrings, JsValue> {
     match nix2js::translate(s, inp_name).map_err(|errors| errors.join("\n")) {
-        Ok((js, map)) => Ok(js_sys::Array::of2(&js.into(), &map.into()).into()),
+        Ok((js, map)) => Ok(JsValue::from(js_sys::Array::of2(&js.into(), &map.into()))
+            .unchecked_into::<TwoStrings>()),
         Err(x) => Err(x.into()),
     }
 }
