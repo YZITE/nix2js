f6bb995c9f5a2006691defb92edcb496a466f529
zseri <zseri.devel@ytrizja.de>
2022-01-05 07:46:16 +0100

    make the mock-runtime reasonable efficient and print origin file of errors

--- mock-runtime.mjs
+++ mock-runtime.mjs
@@ -7,7 +7,7 @@ const REL_PFX = "Relative://";
 const ABS_PFX = "Absolute://";
 
 let expf = (anchor, xpath) => {
-    console.log('called RT.export with anchor=' + anchor + ' path=' + xpath);
+    //console.log('called RT.export with anchor=' + anchor + ' path=' + xpath);
     return anchor + '://' + xpath;
 };
 
@@ -17,6 +17,7 @@ function fmtTdif(tdif) {
     return secs.toString() + "s " + millis.toFixed(3).toString() + "ms";
 }
 
+const rel_path_cache = {};
 const import_cache = {};
 
 function buildRT(opath) {
@@ -26,20 +27,22 @@ function buildRT(opath) {
     return {
         export: expf,
         import: xpath => {
-            console.log(opath + ': called RT.import with path=' + xpath);
             let real_path = null;
+            let tmpp = null;
             if (xpath.startsWith(REL_PFX)) {
+                tmpp = dirnam + '|:' + xpath.slice(REL_PFX.length);
+                if (tmpp in rel_path_cache) {
+                    return rel_path_cache[tmpp];
+                }
                 real_path = path.resolve(dirnam, xpath.slice(REL_PFX.length));
             } else if (xpath.startsWith(ABS_PFX)) {
                 real_path = path.resolve(xpath.slice(ABS_PFX.length));
             } else {
                 throw Error(opath + ": import not supported: " + xpath);
             }
-            console.log('  -> resolved to: ' + real_path);
-            if (real_path in import_cache) {
-                return await import_cache[real_path];
-            }
-            return (async () => {
+            if (!(real_path in import_cache)) {
+                console.log(opath + ': called RT.import with path=' + xpath);
+                console.log('  -> resolved to: ' + real_path);
                 import_cache[real_path] = (async () => {
                     const tstart = process.hrtime();
                     let fdat = null;
@@ -57,12 +60,21 @@ function buildRT(opath) {
                     console.log('  ' + fmtTdif(process.hrtime(tstart)) + '\tloaded');
                     let [trld, srcmap] = translate(fdat, real_path);
                     console.log('  ' + fmtTdif(process.hrtime(tstart)) + '\ttranslated');
-                    let stru = (new Function('nixRt', 'nixBlti', trld))(buildRT(real_path), nixBlti);
+                    let stru;
+                    try {
+                        stru = (new Function('nixRt', 'nixBlti', trld))(buildRT(real_path), nixBlti);
+                    } catch (e) {
+                        console.log(real_path, e);
+                        throw e;
+                    }
                     console.log('  ' + fmtTdif(process.hrtime(tstart)) + '\tevaluated');
                     return stru;
                 })();
-                return await import_cache[real_path];
-            })();
+                if (tmpp !== null) {
+                    rel_path_cache[tmpp] = import_cache[real_path];
+                }
+            }
+            return import_cache[real_path];
         }
     };
 }
